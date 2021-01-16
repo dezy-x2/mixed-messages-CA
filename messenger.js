76b22c95ded63ce2821eba22072b768959ca7bd2
Daniel Desmond <danieldesmond@daniels-air.lan>
2021-01-16 10:59:07 -0500

    added part of speech lists

--- messenger.js
+++ messenger.js
@@ -1,1 +1,3 @@
-console.log("hello world")
+const nouns = ["cup", "dog", "cat", "animal", "computer", "camp", "card", "pencil", "lamp", "book", "stapler", "picture", "wood", "tool", "pin", "note"];
+const adjectives = ["cute", "pretty", "fast", "cool", "nice", "ugly", "dumb", "smart", "windy", "sunny", "cold", "hot", "sticky", "soft", "hard", "annoying"];
+const verbs = ["run", "walk", "bike", "read", "program", "write", "teach", "sleep", "eat", "paint", "work", "sit", "build", "pose", "drink", "charge", "beat"];
